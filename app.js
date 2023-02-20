1c3f9e9621248f5314098aa4d3a7940e31a22ff5
Shimis <78961147+Shinigamiixd@users.noreply.github.com>
2023-02-20 10:53:45 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+require("./config/db")()
+const express = require("express")
+const express_async_handler = require('express-async-handler')
+const webtoken = require("jsonwebtoken")
+const cors = require("cors")
+const bcryptjs = require("bcryptjs")
+
+const app = express()
+
+app.get("/", (req, res, next) => {
+    next()
+})
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on PORT", process.env.PORT)
+})
